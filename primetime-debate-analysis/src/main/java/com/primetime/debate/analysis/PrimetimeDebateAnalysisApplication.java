21d1bf9d12001b5ddb020f028fb17f0a177a7930
vaibhavthapliyal <vaibhav.thapliyal.91@gmail.com>
2020-09-12 13:07:17 +0530

    Initial Commit

---
+++ primetime-debate-analysis/src/main/java/com/primetime/debate/analysis/PrimetimeDebateAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.primetime.debate.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimetimeDebateAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimetimeDebateAnalysisApplication.class, args);
+	}
+
+}
---
+++ primetime-debate-analysis/src/test/java/com/primetime/debate/analysis/PrimetimeDebateAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primetime.debate.analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimetimeDebateAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
